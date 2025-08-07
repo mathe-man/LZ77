20bedb18e88b08a5633f6b575f9db7dffc791eb1
mathe man <mathieumousset6@gmail.com>
2025-08-07 20:56:33 +0000

    Implement compression method

--- src/CompressionManager.cpp
+++ src/CompressionManager.cpp
@@ -1,5 +1,90 @@
-//
-// Created by mathe on 06/08/25.
-//
-
 #include "CompressionManager.h"
+
+#include <iostream>
+#include <span>
+
+
+std::vector<uint8_t> CompressionManager::Compress(std::vector<uint8_t> bytes, size_t search_buffer_size, size_t look_ahead_buffer_size)
+{
+    auto result = std::vector<Pattern>();
+
+    for (size_t i = 0; i < bytes.size(); i++)
+    {
+        auto search_buffer = GetSearchBuffer(bytes, search_buffer_size, i);
+        auto look_ahead_buffer = GetLookAheadBuffer(bytes, look_ahead_buffer_size, i);
+
+        auto pattern = SearchPattern(search_buffer, look_ahead_buffer);
+        result.push_back(pattern);
+        i += pattern.length; // Jump the compressed elements
+    }
+
+}
+
+
+std::vector<uint8_t> CompressionManager::GetSearchBuffer(std::vector<uint8_t> bytes, size_t buffer_size, size_t index)
+{
+    auto result = std::vector<uint8_t>();
+
+    if (buffer_size > index)    // Avoids going into negative index later, returning a smaller buffer
+        buffer_size = index;
+
+    // Set i to the beginning of the search buffer and iterate
+    for (size_t i = index - buffer_size; i < index; i++)
+        result.push_back(bytes[i]);
+
+    return result;
+}
+
+std::vector<uint8_t> CompressionManager::GetLookAheadBuffer(std::vector<uint8_t> bytes, size_t buffer_size, size_t index)
+{
+    auto result = std::vector<uint8_t>();
+
+    if (buffer_size + index - 1 > bytes.size()) // Avoids having an out of range index, returning a smaller buffer
+        buffer_size = bytes.size() - index;
+
+    for (auto i = index; i < index + buffer_size; i++)
+        result.push_back(bytes[i]);
+
+    return result;
+}
+
+Pattern CompressionManager::SearchPattern(const std::vector<uint8_t>& search_buffer, std::vector<uint8_t> look_ahead_buffer)
+{
+    auto view = std::span<uint8_t>(look_ahead_buffer);
+
+    // Start the loop from the biggest size to find the biggest pattern
+    for (size_t i = look_ahead_buffer.size() -1; i > 0; i--)
+    {
+        auto span = view.subspan(0, i);
+
+        auto index_in_buffer = SearchInBuffer(search_buffer, std::vector(span.begin(), span.end()));
+
+        if (index_in_buffer < search_buffer.size()) // Mean that an index as been found
+        {
+            auto distance_to_index = (size_t)std::abs(int(search_buffer.size() - index_in_buffer));
+            return { distance_to_index, span.size(),  look_ahead_buffer[i]};
+        }
+    }
+
+    // This pattern is not present in the search buffer so we only add the next element
+    return {0, 0, look_ahead_buffer[0]};
+}
+
+
+size_t CompressionManager::SearchInBuffer(std::vector<uint8_t> buffer, std::vector<uint8_t> search)
+{
+    for (size_t i = 0; i < buffer.size(); i++)
+    {
+        if (buffer[i] == search[0])
+        {
+            auto equals = true;
+            for (size_t j = 1; j < search.size(); j++)
+                if (buffer[i + j] != search[j])
+                    equals = false;
+            if (equals)
+                return i;
+        }
+    }
+
+    return buffer.size();
+}
--- src/CompressionManager.h
+++ src/CompressionManager.h
@@ -4,14 +4,23 @@
 #include <vector>
 #include <cstddef>
 
+
+struct Pattern
+{
+    size_t offset;
+    size_t length;
+    uint8_t next;
+};
+
+
 class CompressionManager {
 public:
     CompressionManager();
     ~CompressionManager();
 
 
-    [[nodiscard]] std::vector<uint8_t> static Compress(std::vector<uint8_t> bytes, size_t search_buffer_size, size_t look_ahead_buffer_size) const;
-    [[nodiscard]] std::vector<uint8_t> static Decompress(std::vector<uint8_t> bytes) const;
+    [[nodiscard]] std::vector<uint8_t> static Compress(std::vector<uint8_t> bytes, size_t search_buffer_size, size_t look_ahead_buffer_size);
+    [[nodiscard]] std::vector<uint8_t> static Decompress(std::vector<uint8_t> bytes);
 
     [[nodiscard]] static float GetCompressionRatio(const std::vector<uint8_t>& base, const std::vector<uint8_t>& compressed) {
         return GetCompressionRatio(base.size(), compressed.size());
@@ -19,4 +28,11 @@ public:
     [[nodiscard]] static float GetCompressionRatio(const size_t& base_size, const size_t& compressed_size) {
         return static_cast<float>(base_size) / static_cast<float>(compressed_size);
     }
+
+
+// private: // Remove private to use in debug
+    [[nodiscard]] static std::vector<uint8_t> GetSearchBuffer(std::vector<uint8_t> bytes, size_t buffer_size, size_t index);
+    [[nodiscard]] static std::vector<uint8_t> GetLookAheadBuffer(std::vector<uint8_t> bytes, size_t buffer_size, size_t index);
+    [[nodiscard]] static Pattern SearchPattern(const std::vector<uint8_t>& search_buffer, std::vector<uint8_t> look_ahead_buffer);
+    [[nodiscard]] static size_t SearchInBuffer(std::vector<uint8_t> buffer, std::vector<uint8_t> search);
 };
